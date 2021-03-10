d721b691ca32a4d1164cc756c22f24c42edeeda9
Eleanor <Eleanor>
2021-03-11 00:57:44 +0800

    Upated the naming of each selection properly

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(() => {
-const answer = ["Dash", "Buddy", "Momo","Bella","Coco"]
+const answer = ["Dash", "Buddy", "Momo","Bella","Coco", "Goofy", "Bob", "Tiny"]
 const player = []
 
 const shuffle = answer.sort(() => Math.random()- 0.5)
@@ -11,9 +11,10 @@ let compare = () => {
     console.log("Yes");
     } else if(shuffle.some( a => a === player[i])){
         console.log("Answer is close");
-    } else if(a => a !== player[i])
-    console.log("no"); 
+    } else{
+    console.log("No"); 
  }
+}
 }
 compare();
 const logo = $('<h1>').text("Who\'s Back Game")
@@ -22,8 +23,12 @@ $('body').prepend(logo);
 const firstPage = () => {
         $('.page1').show();
         $('.page2').hide();
+        $('.page3').hide();
+        $('.page4').hide();
         $('.page5').hide();
         $('.page6').hide();
+        $('.page7').hide();
+        $('.page8').hide();
 }
 $('body').append(firstPage());
 
@@ -31,34 +36,39 @@ $('body').append(firstPage());
 const play = $('<button>').text("I want to help").addClass("start");
 $('.page1').append(play);
 const handlerPlay = () => {
-        $('.page1').hide();
-        $('.page2').show();
-        $('.page5').hide();
-        $('.page6').hide();
- }
+    $('.page1').hide();
+    $('.page2').show();
+    $('.page3').hide();
+    $('.page4').hide();
+    $('.page5').hide();
+    $('.page6').hide();
+    $('.page7').hide();
+    $('.page8').hide();
+}
 $('.start').on('click', handlerPlay);
 
 
 const knockKnock = $('<button>').text("Knock Knock").addClass("try1");
 $('.page2').append(knockKnock);
 
-$("#dashTreat") .on ('change', () => {
-    let value = $("#dashTreat").val();
+
+$("#treat1") .on ('change', () => {
+    let value = $("#treat1").val();
     player.push(value);
 
 })
-$("#buddyTreat").on ('change', () => {
-    let value = $("#buddyTreat").val();
+$("#treat2").on ('change', () => {
+    let value = $("#treat2").val();
     player.push(value);
 
 })
-$("#momoTreat").on ('change', () => {
-    let value = $("#momoTreat").val();
+$("#treat3").on ('change', () => {
+    let value = $("#treat3").val();
     player.push(value);
 
 })
-$("#cocoTreat").on ('change', () => {
-    let value = $("#cocoTreat").val();
+$("#treat4").on ('change', () => {
+    let value = $("#treat4").val();
     player.push(value);
 
 })
@@ -66,6 +76,34 @@ const hint =() => {
 compare();
 }
  $('.try1').on('click', hint);
+
+const lastPage = $('<button>').text("Knock Knock Last").addClass("lastPage");
+$('.page6').append(lastPage);
+const handlerFinal = () => {
+    if(hint === ["Yes", "Yes,", "Yes"]){
+        $('.page8').show(); 
+        $('.page7').hide();
+       } else {
+        $('.page7').show();
+        $('.page8').hide();
+       }
+    $('.page1').hide();
+    $('.page2').hide();
+    $('.page3').hide();
+    $('.page4').hide();
+    $('.page5').hide();
+    $('.page6').hide();
+   
+ }
+$('.lastPage').on('click', handlerFinal);
+
+const refresh = $('<button>').text("Play again!").addClass("refresh");
+$('.page8').append(refresh);
+const handlerRefresh = () => {
+        location.reload();     
+ }
+$('.refresh').on('click', handlerRefresh);
+
  console.log(shuffle);
  console.log(player);
 });
