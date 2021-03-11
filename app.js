8666e7f76164cd15de0221085d76a3d4767d8a4b
Eleanor <Eleanor>
2021-03-11 17:44:56 +0800

    Almost functional

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 $(() => {
-const answer = ["Dash", "Buddy", "Momo","Bella","Coco", "Goofy", "Bob", "Tiny"]
-const player = []
+    const answer = ["Dash", "Buddy", "Momo", "Bella", "Coco", "Goofy", "Bob", "Tiny"]
+    const player = []
 
-const shuffle = answer.sort(() => Math.random()- 0.5)
-console.log(shuffle);
+    const shuffle = answer.sort(() => Math.random() - 0.5)
+    console.log(shuffle);
 
-let compare = () => {
-    for (let i = 0; i < 5; i++){
-    if (shuffle[i] === player[i]){
-    console.log("Yes");
-    } else if(shuffle.some( a => a === player[i])){
-        console.log("Answer is close");
-    } else{
-    console.log ( "No" ); 
- }
-}
-}
-const logo = $('<h1>').text("Who\'s Back Game")
-$('body').prepend(logo);
+    const logo = $('<h1>').text("Who\'s Back Game")
+    $('body').prepend(logo);
 
-const firstPage = () => {
+    const firstPage = () => {
         $('.page1').show();
         $('.page2').hide();
         $('.page3').hide();
@@ -28,73 +17,13 @@ const firstPage = () => {
         $('.page6').hide();
         $('.page7').hide();
         $('.page8').hide();
-}
-$('body').append(firstPage());
+    }
+    $('body').append(firstPage());
 
 
-const play = $('<button>').text("I want to help").addClass("start");
-$('.page1').append(play);
-const handlerPlay = () => {
-    $('.page1').hide();
-    $('.page2').show();
-    $('.page3').hide();
-    $('.page4').hide();
-    $('.page5').hide();
-    $('.page6').hide();
-    $('.page7').hide();
-    $('.page8').hide();
-}
-$('.start').on('click', handlerPlay);
-
-
-const knockKnock = $('<button>').text("Knock Knock").addClass("try1");
-$('.page2').append(knockKnock);
-
- 
-
-$(".firstTry").on ("change" ,(event) => {
-    let value = $(event.target).val();
-    player.push(value);   
-}) 
-
-
-$(".secondTry").on ("change" ,(event) => {
-    let value = $(event.target).val();
-    player.push(value);   
-}) 
-
-$(".thirdTry").on ("change" ,(event) => {
-    let value = $(event.target).val();
-    player.push(value);   
-}) 
-
-
-$(".fourthTry").on ("change" ,(event) => {
-    let value = $(event.target).val();
-    player.push(value);   
-}) 
-
-$(".fifthTry").on ("change" ,(event) => {
-    let value = $(event.target).val();
-    player.push(value);   
-}) 
-// const hint =() => {
-//     compare();
-// }
-let showHint =$("<h4>").text(compare())
-const hint =() => {
-    $("#window").append (showHint)
-    if (compare() === "Yes"){
-        $('.page8').show();
-        $('.page1').hide();
-        $('.page2').hide();
-        $('.page3').hide();
-        $('.page4').hide();
-        $('.page5').hide();
-        $('.page6').hide();
-        $('.page7').hide();
-    } else {
-        $('.page3').show();
+    const play = $('<button>').text("I want to help").addClass("start");
+    $('.page1').append(play);
+    const handlerPlay = () => {
         $('.page1').hide();
         $('.page2').show();
         $('.page3').hide();
@@ -104,37 +33,110 @@ const hint =() => {
         $('.page7').hide();
         $('.page8').hide();
     }
-}
+    $('.start').on('click', handlerPlay);
 
- $('.try1').on('click', hint);
 
-const lastPage = $('<button>').text("Knock Knock Last").addClass("lastPage");
-$('.page6').append(lastPage);
-const handlerFinal = () => {
-    if(hint === ["Yes", "Yes,", "Yes"]){
-        $('.page8').show(); 
-        $('.page7').hide();
-       } else {
-        $('.page7').show();
-        $('.page8').hide();
-       }
-    $('.page1').hide();
-    $('.page2').hide();
-    $('.page3').hide();
-    $('.page4').hide();
-    $('.page5').hide();
-    $('.page6').hide();
+    const knockKnock = $('<button>').text("Knock Knock").addClass("try1");
+    $('.page2').append(knockKnock);
+
+
+
+    $(".firstTry").on("change", (event) => {
+        let value = $(event.target).val();
+        player.push(value);
+    })
+
+
+    $(".secondTry").on("change", (event) => {
+        let value = $(event.target).val();
+        player.push(value);
+    })
+
+    $(".thirdTry").on("change", (event) => {
+        let value = $(event.target).val();
+        player.push(value);
+    })
+
+
+    $(".fourthTry").on("change", (event) => {
+        let value = $(event.target).val();
+        player.push(value);
+    })
+
+    $(".fifthTry").on("change", (event) => {
+        let value = $(event.target).val();
+        player.push(value);
+    })
+    const compare = () => {
+        let playerGuess = []
+        for (let i = 0; i < 4; i++ && i % 4 === 0) {
+            if (shuffle[i] === player[i]) {
+                playerGuess.push("Yes");
+            } else if (shuffle.some(a => a === player[i])) {
+                playerGuess.push("Answer is close");
+            } else {
+                playerGuess.push("No");
+            }
+        } return playerGuess;
+    }
    
- }
-$('.lastPage').on('click', handlerFinal);
 
-const refresh = $('<button>').text("Play again!").addClass("refresh");
-$('.page8').append(refresh);
-const handlerRefresh = () => {
-        location.reload();     
- }
-$('.refresh').on('click', handlerRefresh);
+    const hint = () => {
+        let showHint = $("<h4>").text(compare())
+        $("#window").append(showHint)
+        console.log(compare());
+        if (compare().includes("Yes","Yes", "Yes", "Yes")) {
+            $('.page8').show();
+            $('.page1').hide();
+            $('.page2').hide();
+            $('.page3').hide();
+            $('.page4').hide();
+            $('.page5').hide();
+            $('.page6').hide();
+            $('.page7').hide();
+        } else {
+            $('.page2').show();
+            $('.page3').show();
+            $('.page1').hide();
+            $('.page3').hide();
+            $('.page4').hide();
+            $('.page5').hide();
+            $('.page6').hide();
+            $('.page7').hide();
+            $('.page8').hide();
+        }
+    console.log(compare());
+    }
 
- 
- console.log(player);
+    $('.try1').on('click', hint);
+
+    const lastPage = $('<button>').text("Knock Knock Last").addClass("lastPage");
+    $('.page6').append(lastPage);
+    const handlerFinal = () => {
+        if (hint === ["Yes", "Yes,", "Yes"]) {
+            $('.page8').show();
+            $('.page7').hide();
+        } else {
+            $('.page7').show();
+            $('.page8').hide();
+        }
+        $('.page1').hide();
+        $('.page2').hide();
+        $('.page3').hide();
+        $('.page4').hide();
+        $('.page5').hide();
+        $('.page6').hide();
+
+    }
+    $('.lastPage').on('click', handlerFinal);
+
+    const refresh = $('<button>').text("Play again!").addClass("refresh");
+    $('.page8').append(refresh);
+    const handlerRefresh = () => {
+        location.reload();
+    }
+    $('.refresh').on('click', handlerRefresh);
+
+
+    console.log(player);
 });
