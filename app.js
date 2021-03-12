94abb0cc305cbbcf4d36d1322cfa0acb2e2e9bc4
Eleanor <Eleanor>
2021-03-12 08:39:55 +0800

    Almost done except adjusting css

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 $(() => {
     const answer = ["Dash", "Buddy", "Momo", "Bella", "Coco", "Goofy", "Bob", "Tiny"]
-    const player = []
+    let player = []
 
     const shuffle = answer.sort(() => Math.random() - 0.5)
     console.log(shuffle);
 
-    const logo = $('<h1>').text("Who\'s Back Game")
-    $('body').prepend(logo);
+    // const logo = $('<h1>').text("Who\'s Back Game")
+    // $('body').prepend(logo);
 
     const firstPage = () => {
         $('.page1').show();
         $('.page2').hide();
-        $('.page3').hide();
-        $('.page4').hide();
-        $('.page5').hide();
-        $('.page6').hide();
         $('.page7').hide();
         $('.page8').hide();
+        $('.try1').hide();
+        $('.page2').hide();
+        $(".secondTry").hide();
+        $('.try2').hide();
+        $('.try3').hide();
+        $('.try4').hide();
+        $('.try5').hide();
+        $(".thirdTry").hide();
+        $(".fourthTry").hide();
+        $(".fifthTry").hide();
+        $(".hint2").hide();
+        $(".hint3").hide();
+        $(".hint4").hide();
+        $(".hint5").hide();
+        $('.page7').hide();
     }
     $('body').append(firstPage());
 
@@ -26,18 +37,35 @@ $(() => {
     const handlerPlay = () => {
         $('.page1').hide();
         $('.page2').show();
-        $('.page3').hide();
-        $('.page4').hide();
-        $('.page5').hide();
-        $('.page6').hide();
         $('.page7').hide();
         $('.page8').hide();
+        $('.try1').show();
+        $(".secondTry").hide();
+        $('.try2').hide();
+        $('.try3').hide();
+        $('.try4').hide();
+        $('.try5').hide();
+        $(".thirdTry").hide();
+        $(".fourthTry").hide();
+        $(".fifthTry").hide();
+        $(".hint2").hide();
+        $(".hint3").hide();
+        $(".hint4").hide();
+        $(".hint5").hide();
     }
     $('.start').on('click', handlerPlay);
 
 
     const knockKnock = $('<button>').text("Knock Knock").addClass("try1");
     $('.page2').append(knockKnock);
+    const knockKnock1 = $('<button>').text("Knock Knock").addClass("try2");
+    $('.page2').append(knockKnock1);
+    const knockKnock2 = $('<button>').text("Knock Knock").addClass("try3");
+    $('.page2').append(knockKnock2);
+    const knockKnock3 = $('<button>').text("Knock Knock").addClass("try4");
+    $('.page2').append(knockKnock3);
+    const knockKnock4 = $('<button>').text("Knock Knock").addClass("try5");
+    $('.page2').append(knockKnock4);
 
 
 
@@ -47,26 +75,30 @@ $(() => {
     })
 
 
-    // $(".secondTry").on("change", (event) => {
-    //     let value = $(event.target).val();
-    //     player.push(value);
-    // })
+    $(".secondTry").on("change", (event) => {
+        let player=[];
+        let value = $(event.target).val();
+        player.push(value);
+    })
 
-    // $(".thirdTry").on("change", (event) => {
-    //     let value = $(event.target).val();
-    //     player.push(value);
-    // })
+    $(".thirdTry").on("change", (event) => {
+        let player=[];
+        let value = $(event.target).val();
+        player.push(value);
+    })
 
 
-    // $(".fourthTry").on("change", (event) => {
-    //     let value = $(event.target).val();
-    //     player.push(value);
-    // })
+    $(".fourthTry").on("change", (event) => {
+        let player=[];
+        let value = $(event.target).val();
+        player.push(value);
+    })
 
-    // $(".fifthTry").on("change", (event) => {
-    //     let value = $(event.target).val();
-    //     player.push(value);
-    // })
+    $(".fifthTry").on("change", (event) => {
+        let player=[];
+        let value = $(event.target).val();
+        player.push(value);
+    })
     const compare = () => {
         let playerGuess = []
         for (let i = 0; i < 4; i++ && i % 4 === 0) {
@@ -80,31 +112,6 @@ $(() => {
         } return playerGuess;
     }
    
-// let window =[]
-// $.each($(".hint1").children, function(element){
-//     window.push($(this));
-// });
-// $(".hint1").children.each=()=>{
-//         window.push($(this));
-// }
-// console.log(window);
-    //     let window =[]
-    //     $.each($(".hint1").children, function(element){
-    // window.push($(this));
-    //    });
-    //     let yes = "Yes"
-    //     let maybe = "Answer is close"
-    //     if(yes === true) {
-    //         $(".hint1").children[i].style.backgroundColor = "deepskyblue";
-    //     } else if (maybe === true) {
-    //         $(".hint1").children[i].style.backgroundColor = "hotpink";
-    //     } else{
-    //         return;
-    //     }
-const test =()=>{
-
-}
-test();
 
     const hint = () => {    
     let showHint = $("<h4>").text(compare())
@@ -113,49 +120,148 @@ let ans = ["Yes", "Yes", "Yes", "Yes"]
 let ansB = compare()
     if (JSON.stringify(ans) === JSON.stringify(ansB)){
         console.log("hello");
-        $('.page8').hide();
         $('.page8').show();
         $('.page1').hide();
         $('.page2').hide();
-        $('.page3').hide();
-        $('.page4').hide();
-        $('.page5').hide();
-        $('.page6').hide();
         $('.page7').hide();
     }else {
+        $("#instructions").hide();
         $('.try1').hide();
         $('.page2').show();
-        $('.page3').show();
-        $('.page1').hide();
-        $('.page3').hide();
-        $('.page4').hide();
-        $('.page5').hide();
-        $('.page6').hide();
+        $(".secondTry").show();
+        $('.try2').show();
+        $('.try3').hide();
+        $('.try4').hide();
+        $('.try5').hide();
+        $(".thirdTry").hide();
+        $(".fourthTry").hide();
+        $(".fifthTry").hide();
+        $(".hint2").hide();
+        $(".hint3").hide();
+        $(".hint4").hide();
+        $(".hint5").hide();
         $('.page7').hide();
     }
     console.log(compare());
     }
-    $('.try1').on('click', hint);
-
-    const lastPage = $('<button>').text("Knock Knock Last").addClass("lastPage");
-    $('.page6').append(lastPage);
-    const handlerFinal = () => {
-        if (hint === ["Yes", "Yes,", "Yes"]) {
+    const hint1 = () => {    
+        let showHint = $("<h4>").text(compare())
+        $("#window1").append(showHint)
+    let ans = ["Yes", "Yes", "Yes", "Yes"]
+    let ansB = compare()
+        if (JSON.stringify(ans) === JSON.stringify(ansB)){
+            console.log("hello");
             $('.page8').show();
+            $('.page1').hide();
+            $('.page2').hide();
             $('.page7').hide();
-        } else {
-            $('.page7').show();
-            $('.page8').hide();
+        }else {
+            $("#instructions").hide();
+            $('.try1').hide();
+            $('.page2').show();
+            $(".secondTry").show();
+            $('.try2').hide();
+            $('.try3').show();
+            $('.try4').hide();
+            $('.try5').hide();
+            $(".thirdTry").show();
+            $(".fourthTry").hide();
+            $(".fifthTry").hide();
+            $(".hint2").hide();
+            $(".hint3").hide();
+            $(".hint4").hide();
+            $(".hint5").hide();
+            $('.page7').hide();
         }
-        $('.page1').hide();
-        $('.page2').hide();
-        $('.page3').hide();
-        $('.page4').hide();
-        $('.page5').hide();
-        $('.page6').hide();
-
-    }
-    $('.lastPage').on('click', handlerFinal);
+        console.log(compare());
+        }
+        const hint2 = () => {    
+            let showHint = $("<h4>").text(compare())
+            $("#window1").append(showHint)
+        let ans = ["Yes", "Yes", "Yes", "Yes"]
+        let ansB = compare()
+            if (JSON.stringify(ans) === JSON.stringify(ansB)){
+                console.log("hello");
+                $('.page8').show();
+                $('.page1').hide();
+                $('.page2').hide();
+                $('.page7').hide();
+            }else {
+                $("#instructions").hide();
+                $('.try1').hide();
+                $('.page2').show();
+                $(".secondTry").show();
+                $('.try2').hide();
+                $('.try3').hide();
+                $('.try4').show();
+                $('.try5').hide();
+                $(".thirdTry").show();
+                $(".fourthTry").show();
+                $(".fifthTry").hide();
+                $(".hint2").hide();
+                $(".hint3").hide();
+                $(".hint4").hide();
+                $(".hint5").hide();
+                $('.page7').hide();
+            }
+            console.log(compare());
+            }
+            const hint3 = () => {    
+                let showHint = $("<h4>").text(compare())
+                $("#window1").append(showHint)
+            let ans = ["Yes", "Yes", "Yes", "Yes"]
+            let ansB = compare()
+                if (JSON.stringify(ans) === JSON.stringify(ansB)){
+                    console.log("hello");
+                    $('.page8').show();
+                    $('.page1').hide();
+                    $('.page2').hide();
+                    $('.page7').hide();
+                }else {
+                    $("#instructions").hide();
+                    $('.try1').hide();
+                    $('.page2').show();
+                    $(".secondTry").show();
+                    $('.try2').hide();
+                    $('.try3').hide();
+                    $('.try4').hide();
+                    $('.try5').show();
+                    $(".thirdTry").show();
+                    $(".fourthTry").show();
+                    $(".fifthTry").show();
+                    $(".hint2").show();
+                    $(".hint3").show();
+                    $(".hint4").show();
+                    $(".hint5").hide();
+                    $('.page7').hide();
+                }
+                console.log(compare());
+                }
+                    const hint4 = () => {    
+                        let showHint = $("<h4>").text(compare())
+                        $("#window1").append(showHint)
+                    let ans = ["Yes", "Yes", "Yes", "Yes"]
+                    let ansB = compare()
+                        if (JSON.stringify(ans) === JSON.stringify(ansB)){
+                            console.log("hello");
+                            $('.page8').show();
+                            $('.page1').hide();
+                            $('.page2').hide();
+                            $('.page7').hide();
+                        }else {
+                             
+                            $('.page8').show();
+                            $('.page1').hide();
+                            $('.page2').hide();
+                            $('.page7').hide();
+                        }
+                        console.log(compare());
+                        }        
+    $('.try1').on('click', hint);
+    $('.try2').on('click', hint1);
+    $('.try3').on('click', hint2);
+    $('.try4').on('click', hint3);
+    $('.try5').on('click', hint4);
 
     const refresh = $('<button>').text("Play again!").addClass("refresh");
     $('.page8').append(refresh);
